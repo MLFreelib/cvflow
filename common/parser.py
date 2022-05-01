4c0c410f5f822147d5781832adcee1199a6aafda
paNoNi <novgorodcevmp@gmail.com>
2022-05-01 22:20:53 +0300

    Added the device argument for the parser

--- common/parser.py
+++ common/parser.py
@@ -7,5 +7,6 @@ argparser.add_argument('--videofile', required=False)
 argparser.add_argument('-c', '--confidence', required=False)
 argparser.add_argument('-f', '--font', required=False)
 argparser.add_argument('--tsize', required=False)
+argparser.add_argument('-d', '--device', required=False)
 
 args = vars(argparser.parse_args())
