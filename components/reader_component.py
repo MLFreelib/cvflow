0eea6ff6f6acf0746e875a02a048e184820706ad
paNoNi <novgorodcevnp@gmail.com>
2022-05-14 20:15:37 +0300

    Added ImageReader

--- components/reader_component.py
+++ components/reader_component.py
@@ -1,3 +1,7 @@
+import os
+import time
+from typing import List, Union
+
 import cv2
 import numpy as np
 
@@ -98,3 +102,47 @@ class VideoReader(ReaderBase):
     def stop(self):
         r""" Clearing memory. """
         self.__cap_send.release()
+
+
+class ImageReader(ReaderBase):
+    r""" Reads the image or images from directory.
+        :param: path: str
+                path to image or directory with images
+        :param: name: str
+                name of component
+    """
+    def __init__(self, path: str, name: str):
+        super().__init__(path, name)
+        self.__files: Union[List[str], None] = None
+        self.__last_file: int = 0
+        self.__time_step = 1
+        self.__last_iter = None
+
+    def set_time_step(self, step: int = 10):
+        r""" Pause between image changes.
+            :param step: int
+                    seconds
+        """
+        self.__time_step = step
+
+    def run(self):
+        self.__last_iter = time.time()
+        if os.path.isfile(self._path):
+            self.__files = [self._path]
+        elif os.path.isdir(self._path):
+            self.__files = os.listdir(self._path)
+        else:
+            ValueError(f'Expected path to image or directory with images, actual {self._path}')
+
+    def read(self) -> np.array:
+        if (time.time() - self.__last_iter) / 1000 < self.__time_step:
+            last_file = self.__last_file
+        else:
+            last_file = max(self.__last_file - 1, 0)
+        path = os.path.join(self._path, self.__files[last_file])
+        frame = cv2.imread(path)
+        self.__last_file = self.__last_file + 1 if self.__last_file + 1 < len(self.__files) else self.__last_file
+        self.__last_iter = time.time()
+        return frame
+
+
