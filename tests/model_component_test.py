76104e2fb2d3acb8c8a3343810908d5414bbb51a
s70c3 <mari.s70c3@gmail.com>
2022-07-23 21:39:47 +0300

    Bugfix. Update requirements.txt.

--- tests/model_component_test.py
+++ tests/model_component_test.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import sys
+sys.path.append('../')
 
 import cv2
 import numpy as np
@@ -123,20 +125,20 @@ class ModelClassificationTest(unittest.TestCase):
         del self.resnet50
 
 
-
 class ModelDepthTest(unittest.TestCase):
 
     def setUp(self):
         self.msn2d = MSNet2D()
         self.msn2d.eval()
         self.model = ModelDepth('depht_test', self.msn2d)
-        self.model.set_device('cuda')
+        self.model.set_device('cpu')
 
         meta_batch = MetaBatch('test_batch')
         for i in range(2):
             src_name = f'test_frame{i}'
             self.model.add_source(src_name)
-            frame = cv2.imread(filename=os.path.join(os.path.dirname(__file__), 'test_data', 'mouse.jpeg'))
+            frame = cv2.imread(filename=os.path.join(os.path.dirname(__file__), 'test_data', f'stereo{i}.png'))
+            frame = cv2.resize(frame, (512, 960))
             meta_frame = _to_meta_frame(frame=frame, src_name=src_name)
             meta_batch.add_meta_frame(meta_frame)
             meta_batch.add_frames(src_name, meta_frame.get_frame())
