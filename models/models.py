b2c1aa56f6547599efbd7da3fb15cfd64f350e10
s70c3 <mari.s70c3@gmail.com>
2023-06-11 02:02:05 +0300

    Fix in dependencies.

--- models/models.py
+++ models/models.py
@@ -1,15 +1,11 @@
 import os
 from typing import Union
 
-from ultralytics import YOLO
-
 from models.blocks import *
 from models.defects.blocks import OutBlock
 from models.defects.ssd300 import SSD300
 from models.defects.vgg19 import InputRescale
 from models.preprocessing import *
-from models.stereo.blocks import GANetInputBlock, GANetBackbone, GANetOutputBlock, DepthOutput, MobileStereoNetBackbone, \
-    MobileStereoNetInputBlock
 
 
 class ModelBuilder(nn.Module):
@@ -151,10 +147,10 @@ def defects_model(path_to_templates=None, emb_size: int = 512, weights=None, is_
         output_block=OutBlock(-1, -1).to(device)
     )
 
-def mobilestereonet(weights = None, is_train=False, device='cuda'):
+def mobilestereonet(weights = None):
     input_block = MobileStereoNetInputBlock()
-    backbone = MobileStereoNetBackbone(training=is_train)
-    output_block = DepthOutput(training=is_train)
+    backbone = MobileStereoNetBackbone()
+    output_block = DepthOutput()
     if weights:
         weight_index = input_block.import_weights(weights)
         weight_index = backbone.import_weights(weights, weight_index)
@@ -182,15 +178,6 @@ def ganet(weights = None):
     )
     torch.nn.DataParallel(model)
     return model
-
-def yolov8(weights=None):
-    return ModelBuilder(
-        input_block=Block(1, 1),
-        backbone=YOLO(weights),
-        output_block=Block(1, 1),
-    )
-
-import segmentation_models_pytorch as smp
 
 def unet(weights=None):
     model = torch.load(weights, map_location=torch.device('cpu'))
